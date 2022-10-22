7cb908a4ff4077244004e894cf2183d1ccc8b971
JB Bell <tquidca@gmail.com>
2022-10-21 17:11:32 -0700

    Broke as hell

--- src/dice.rs
+++ src/dice.rs
@@ -1,40 +1,56 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use rand::{thread_rng, Rng};
+
+fn rand_int(n: i32) -> i32 {
+    let mut rng = thread_rng();
+    rng.gen_range(0..n)
+}
+
+fn roll_dx(faces: Vec<Face>) -> OneDie {
+    let face_index = rand_int(faces.len() as i32);
+    OneDie { face_index, faces }
+}
+
+#[derive(Debug)]
+struct Face {
+    face_tuple: (i32, String),
+}
 
 #[derive(Debug)]
 pub struct OneDie {
-    value: i64,
-    faces: Vec<i64>,
+    face_index: i32,
+    faces: Vec<Face>,
 }
 
-impl OneDie {
-    // pub fn roll(mut die: OneDie<'die>) -> Self {
-    //     let mut rng = thread_rng();
-    //     die.value = die.faces.choose(&mut rng).as_mut();
-    //     die
-    // }
-
-    pub fn new(faces: Vec<i64>) -> Self {
-        let mut rng = thread_rng();
-        let value = *faces.choose(&mut rng).unwrap();
-        OneDie { faces, value }
+impl<'die> OneDie {
+    pub fn new(faces: Vec<Face>, roll_fn: &'die dyn Fn(Vec<Face>) -> PoolOrDie) -> PoolOrDie {
+        roll_fn(faces)
     }
 }
 
 #[derive(Debug)]
-pub struct Pool {
-    dice: Vec<OneDie>,
+pub struct Pool<'pool> {
+    dice: Vec<&'pool OneDie>,
+    pools: Vec<&'pool Pool<'pool>>,
     //     placement: Area,
     //     owner: Actor,
 }
 
+enum PoolOrDie {
+    Pool,
+    OneDie,
+}
+
 impl Pool {
-    pub fn new(faces: i64, number: i64) -> Self {
+    pub fn new(rollables: Vec<(OneDie, &dyn Fn(Vec<Face>) -> PoolOrDie)>) -> Self {
         let mut dice: Vec<OneDie> = Vec::new();
-        for _ in 1..=number {
-            let die = OneDie::new((1..=faces).collect());
-            dice.push(die);
+        let mut pools: Vec<Pool> = Vec::new();
+        for (die, roll_fn) in rollables {
+            let pool_or_die = OneDie::new(die.faces, &roll_fn);
+            match pool_or_die {
+                PoolOrDie::OneDie => dice.push(&pool_or_die),
+                PoolOrDie::Pool => pools.push(&pool_or_die),
+            }
         }
-        Pool { dice }
+        Pool { dice, pools }
     }
 }
