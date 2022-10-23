c099404af689ba96fb21b3fd685e545b355cc32e
JB Bell <tquidca@gmail.com>
2022-10-23 12:37:33 -0700

    Actually runs!

--- src/dice.rs
+++ src/dice.rs
@@ -1,37 +1,52 @@
 use rand::{thread_rng, Rng};
 
-fn rand_int(n: i32) -> i32 {
+fn rand_uint(n: u32) -> u32 {
     let mut rng = thread_rng();
     rng.gen_range(0..n)
 }
 
-fn roll_dx(faces: Vec<Face>) -> OneDie {
-    let face_index = rand_int(faces.len() as i32);
-    OneDie { face_index, faces }
+#[derive(Debug, Clone)]
+pub struct Face (pub i32, pub String);
+
+trait Roll {
+    type Die;
+    fn roll(&self) -> Dice;
 }
 
 #[derive(Debug)]
-struct Face {
-    face_tuple: (i32, String),
+struct Die {
+    faces: Vec<Face>,
+    face_index: u32,
+}
+
+impl Die {
+    fn new(faces: &Vec<Face>) -> Self {
+        Self {
+            faces: faces.to_vec(),
+            face_index: rand_uint(faces.len() as u32),
+        }
+    }
 }
 
 #[derive(Debug)]
-pub enum Pool<'pool> {
-    OneDie { faces: Vec<Face>, roll_fn: &dyn Fn(Vec<Face>) -> Pool },
-    MultiDice { dice: Vec<Pool<'pool>> },
+enum Dice {
+    One(Die),
+    Many(Vec<Die>),
 }
 
-impl<'pool> Pool<'pool> {
-    fn roll(&self) -> Self {
-        match self {
-            Pool::OneDie { faces, roll_fn } => roll_fn(faces),
-            Pool::MultiDice {
-                let results: Vec<Pool>,
-                for (die, roll_fn) in dice {
-                    results.push(roll_fn(faces))
-                }
-                results
-            },
+#[derive(Debug)]
+pub struct Pool {
+    dice: Vec<Dice>,
+}
+
+impl<'pool> Pool {
+    pub fn new(dice: Vec<Vec<Face>>) -> Self {
+        let mut rolls: Vec<Dice> = vec!();
+        for die in dice {
+            rolls.push(Dice::One(Die::new(&die)))
+        }
+        Self {
+            dice: rolls,
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 pub mod dice;
 
-pub use dice::Pool;
+pub use dice::{Face, Pool};
 
 fn main() {
-    println!("Pool: {:?}", Pool::new(6, 3))
+    println!("Pool: {:?}", Pool::new(vec!(vec!(
+        Face(1, "1".to_string()),
+        Face(2, "2".to_string()),
+        Face(3, "3".to_string()),
+        Face(4, "4".to_string()),
+        Face(5, "5".to_string()),
+        Face(6, "6".to_string()),
+    ))));
 }
