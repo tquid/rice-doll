e8dfad0ff2e42442b5066b0d25da06348550c862
JB Bell <tquidca@gmail.com>
2022-11-12 13:11:16 -0800

    WTF am I even doing

--- src/dice.rs
+++ src/dice.rs
@@ -1,71 +1,73 @@
 use core::fmt::{Display, Formatter, Result};
+use std::rc::Rc;
 
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 
 fn rand_uint(n: usize) -> usize {
     let mut rng = thread_rng();
     rng.gen_range(0..n)
 }
 
-fn explode_roll(faces: &Vec<Face>) -> Dice {
-    let max: usize = faces.len();
-    let mut dice: Vec<Die> = vec!();
+fn roll<'die, 'face> (die: &'die Die) -> &'die Die<'die, 'face> {
+    die.shown_face = die.faces.choose(&mut rand::thread_rng());
+    die
+}
+
+fn explode_roll<'die, 'faces> (die: &'die Die, explode_faces: &'faces Vec<Face>) -> &'die Die<'die, 'faces> {
+    let max: usize = die.faces.len();
+    die.shown_face = die.faces.choose(&mut rand::thread_rng());
     loop {
-        let face_index = rand_uint(max);
-        let new_die: Die = Die {
-            faces: faces.to_vec(),
-            face_index,
-        };
-        dice.push(new_die);
-        if face_index + 1 != max {
+        if !explode_faces.contains(die.shown_face.unwrap()) {
             break;
         }
         println!("Pop!");
+        die.next_die = explode_roll(&Die::new(&die.faces, die.roll_fn), explode_faces);
     }
-    Dice(dice)
+    &die
 }
 
 pub fn mould_int_die(num_faces: i32) -> Vec<Face> {
     let mut faces: Vec<Face> = vec!();
     for value in 1..=num_faces {
-        faces.push(Face(value, value.to_string()));
+        faces.push(Face(value.to_string()));
     }
     faces
 }
 
-#[derive(Debug, Clone)]
-pub struct Face (pub i32, pub String);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Face (pub String);
 
 trait Roll {
-    type Die;
-    fn roll(&self) -> Dice;
+    fn roll(&self) -> Self;
 }
 
-#[derive(Debug)]
-struct Die {
+struct Die<'die, 'face> {
     faces: Vec<Face>,
-    face_index: usize,
+    shown_face: Option<&'face Face>,
+    next_die: Option<Box<Die<'die, 'face>>>,
+    roll_fn: Box<dyn Fn(&Self) -> &Self>,
 }
 
-impl Die {
-    fn new(faces: &Vec<Face>, roll_fn: &dyn Fn(&Vec<Face>) -> Dice) -> Dice {
-        roll_fn(faces)
+impl<'die, 'face> Die<'die, 'face> {
+    fn new(faces: Vec<Face>, roll_fn: Box<dyn Fn(&Die) -> &'die Die<'die, 'face>>) -> Self {
+        Die {
+            faces,
+            shown_face: None,
+            next_die: None,
+            roll_fn,
+        }
     }
 }
 
-#[derive(Debug)]
-struct Dice (Vec<Die>);
-
-#[derive(Debug)]
-pub struct Pool {
-    dice: Vec<Dice>,
+pub struct Pool<'pool, 'dice> {
+    dice: Vec<Die<'pool, 'dice>>,
 }
 
-impl Pool {
-    pub fn new(dice: Vec<Vec<Face>>) -> Self {
-        let mut rolls: Vec<Dice> = vec!();
+impl<'pool, 'dice> Pool<'pool, 'dice> {
+    pub fn new(dice: Vec<Die>>) -> Self {
+        let mut rolls: Vec<Die> = vec!();
         for die in dice {
-            rolls.push(Die::new(&die, &explode_roll))
+            rolls.push(explode_roll(die, explode_faces))
         }
         Self {
             dice: rolls,
@@ -76,20 +78,6 @@ impl Pool {
 impl Display for Die {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "d{}: {}", self.faces.len(), self.faces[self.face_index].1)
-    }
-}
-
-impl Display for Dice {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let mut output: Vec<String> = vec!();
-        for die in &self.0 {
-            output.push(std::format!("{}", die));
-        }
-        if output.len() == 1 {
-            write!(f, "{}", output[0])
-        } else {
-            write!(f, "[{}]", output.join(", "))
-        }
     }
 }
 
