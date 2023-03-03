b31d05b1669efac101b9fa26b43e026142f7abd5
JB Bell <tquidca@gmail.com>
2023-03-03 07:19:06 -0800

    Complete dice re-design

--- src/dice.rs
+++ src/dice.rs
@@ -1,108 +1,70 @@
-use core::fmt::{Display, Formatter, Result};
-
 use rand::{seq::SliceRandom, thread_rng, Rng};
 
-fn rand_uint(n: usize) -> usize {
-    let mut rng = thread_rng();
-    rng.gen_range(0..n)
+type DieValue = i16;
+type DieSize = usize;
+
+#[derive(Debug, Clone)]
+pub struct Face {
+    sign: String,
+    value: DieValue,
 }
 
-fn roll<'die, 'face> (die: &'die Die) -> &'die Die<'die, 'face> {
-    die.shown_face = die.faces.choose(&mut rand::thread_rng());
-    die
-}
-
-fn explode_roll<'die, 'faces> (die: &'die Die, explode_faces: &'faces Vec<Face>) -> &'die Die<'die, 'faces> {
-    let max: usize = die.faces.len();
-    die.shown_face = die.faces.choose(&mut rand::thread_rng());
-    loop {
-        if !explode_faces.contains(die.shown_face.unwrap()) {
-            break;
-        }
-        println!("Pop!");
-        die.next_die = explode_roll(&Die::new(&die.faces, die.roll_fn), explode_faces);
+impl Face {
+    fn new(sign: String, value: DieValue) -> Self {
+        Self { sign, value }
     }
-    &die
 }
 
-pub fn mould_int_die(num_faces: i32) -> Vec<Face> {
-    let mut faces: Vec<Face> = vec!();
-    for value in 1..=num_faces {
-        faces.push(Face(value.to_string()));
+impl From<DieValue> for Face {
+    fn from(value: DieValue) -> Self {
+        Face::new(value.to_string(), value)
     }
-    faces
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Face (pub String);
-
-trait Roll {
-    fn roll(&self) -> Self;
-}
-
-struct DieBuilder {
+#[derive(Debug)]
+pub struct Die {
     faces: Vec<Face>,
     shown_face: Option<Face>,
-    next_die: Option<Box<Die>>,
-    roll_fn: Option<Box<dyn Fn(&Self) -> &Self>>,
 }
 
-impl DieBuilder {
-    fn new(faces: Vec<Face>) -> Self {
+// Die::new(Vec<Face>) to create a fully custom die with arbitrary signs and
+// values
+impl Die {
+    pub fn new(faces: Vec<Face>) -> Self {
         Self {
             faces,
             shown_face: None,
-            next_die: None,
-            roll_fn: None,
         }
     }
-}
-struct Die {
-    faces: Vec<Face>,
-    shown_face: Face,
-    next_die: Option<Box<Die>>,
-    roll_fn: Box<dyn Fn(&Self) -> &Self>,
-}
 
-impl<'die> Die<'die, 'die> {
-    fn new(faces: Vec<Face>, roll_fn: Box<dyn Fn(&Die) -> &'die Die<'die, 'die>>) -> Self {
-        Die {
-            faces,
-            shown_face: None,
-            next_die: None,
-            roll_fn,
-        }
+    pub fn roll(&mut self)  -> &mut Self {
+        self.shown_face = self.faces.choose(&mut rand::thread_rng()).cloned();
+        self
+    }
+
+    pub fn get_face(self) -> Face {
+        self.shown_face.unwrap()
     }
 }
 
-pub struct Pool<'pool, 'dice> {
-    dice: Vec<Die<'pool, 'dice>>,
-}
-
-impl<'pool> Pool<'pool, 'pool> {
-    pub fn new(dice: Vec<Die>>) -> Self {
-        let mut rolls: Vec<Die> = vec!();
-        for die in dice {
-            rolls.push(explode_roll(die, explode_faces))
+// Die::from(Vec<DieValue>) to create a die with arbitrary integer faces
+impl From<Vec<DieValue>> for Die {
+    fn from(numbers: Vec<DieValue>) -> Self {
+        let mut faces = Vec::<Face>::new();
+        for n in numbers {
+            faces.push(Face::from(n))
         }
         Self {
-            dice: rolls,
+            faces,
+            shown_face: None,
         }
     }
 }
 
-impl Display for Die {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "d{}: {}", self.faces.len(), self.faces[self.face_index].1)
-    }
-}
-
-impl Display for Pool {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let mut output: Vec<String> = vec!();
-        for dice in &self.dice {
-            output.push(std::format!("{}", dice));
-        }
-        write!(f, "{}", output.join(", "))
+// Use Die::from(DieSize) to create a typical die numbered 1..n
+impl From<DieSize> for Die {
+    fn from(size: DieSize) -> Self {
+        let sides: Vec<DieValue> = (1..size as DieValue).collect();
+        Die::from(sides)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 pub mod dice;
 pub mod parser;
 
-pub use dice::{mould_int_die as d, Pool};
+pub use dice::{Die};
 pub use parser::parse;
 
 fn main() {
-    println!("{}", Pool::new(vec!(d(4), d(6), d(8), d(10), d(12), d(20))));
-    parse();
+    // println!("{}", Pool::new(vec!(d(4), d(6), d(8), d(10), d(12), d(20))));
+    // parse();
+    let mut d6 = Die::from(6);
+    d6.roll();
+    println!("Face showing: {:?}", d6.get_face());
 }
