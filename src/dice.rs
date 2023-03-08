3ccef4c5d3767b7aebdd66dbad8f46c876643f76
JB Bell <tquidca@gmail.com>
2023-03-08 14:28:51 -0800

    Remove idiosyncratic `From` overloading and add tests

--- src/dice.rs
+++ src/dice.rs
@@ -3,7 +3,7 @@ use rand::{seq::SliceRandom, thread_rng, Rng};
 type DieValue = i16;
 type DieSize = usize;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Eq, Clone, PartialEq)]
 pub struct Face {
     sign: String,
     value: DieValue,
@@ -13,10 +13,8 @@ impl Face {
     fn new(sign: String, value: DieValue) -> Self {
         Self { sign, value }
     }
-}
 
-impl From<DieValue> for Face {
-    fn from(value: DieValue) -> Self {
+    fn int(value: DieValue) -> Self {
         Face::new(value.to_string(), value)
     }
 }
@@ -37,34 +35,58 @@ impl Die {
         }
     }
 
-    pub fn roll(&mut self)  -> &mut Self {
-        self.shown_face = self.faces.choose(&mut rand::thread_rng()).cloned();
-        self
-    }
-
-    pub fn get_face(self) -> Face {
-        self.shown_face.unwrap()
-    }
-}
-
-// Die::from(Vec<DieValue>) to create a die with arbitrary integer faces
-impl From<Vec<DieValue>> for Die {
-    fn from(numbers: Vec<DieValue>) -> Self {
+    pub fn range(numbers: Vec<DieValue>) -> Self {
         let mut faces = Vec::<Face>::new();
         for n in numbers {
-            faces.push(Face::from(n))
+            faces.push(Face::int(n))
         }
         Self {
             faces,
             shown_face: None,
         }
     }
+
+    pub fn int(size: DieSize) -> Self {
+        let sides: Vec<DieValue> = (1..(size + 1) as DieValue).collect();
+        Die::range(sides)
+    }
+
+    pub fn roll(&mut self)  -> &mut Self {
+        self.shown_face = self.faces.choose(&mut rand::thread_rng()).cloned();
+        self
+    }
+
+    pub fn get_shown_face(&self) -> &Face {
+        self.shown_face.as_ref().unwrap()
+    }
+
+    pub fn get_size(&self) -> DieSize {
+        self.faces.len()
+    }
+
+    pub fn get_faces(&self) -> &Vec<Face> {
+        self.faces.as_ref()
+    }
 }
 
-// Use Die::from(DieSize) to create a typical die numbered 1..n
-impl From<DieSize> for Die {
-    fn from(size: DieSize) -> Self {
-        let sides: Vec<DieValue> = (1..size as DieValue).collect();
-        Die::from(sides)
+
+#[cfg(test)]
+pub mod test {
+    use super::{Die, Face};
+    #[test]
+    pub fn correct_size_from_integer() {
+        let d6: Die = Die::int(6);
+        assert!(Die::get_size(&d6) == 6);
+    }
+
+    #[test]
+    pub fn correct_die_from_integer() -> () {
+        let d6 = Die::int(6);
+        assert!(d6.get_faces() == &vec![Face { sign: "1".to_owned(), value: 1 },
+                                        Face { sign: "2".to_owned(), value: 2 },
+                                        Face { sign: "3".to_owned(), value: 3 },
+                                        Face { sign: "4".to_owned(), value: 4 },
+                                        Face { sign: "5".to_owned(), value: 5 },
+                                        Face { sign: "6".to_owned(), value: 6 }]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ pub use parser::parse;
 fn main() {
     // println!("{}", Pool::new(vec!(d(4), d(6), d(8), d(10), d(12), d(20))));
     // parse();
-    let mut d6 = Die::from(6);
+    let mut d6 = Die::int(6);
     d6.roll();
-    println!("Face showing: {:?}", d6.get_face());
+    println!("Face showing: {:?}", d6.get_shown_face());
+    println!("This is a {}-sided die", d6.get_size());
+    println!("Faces: {:?}", d6.get_faces())
 }
